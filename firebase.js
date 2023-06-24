aee9bd95b77463a3073859dbcd9e40f4c199b80d
Shivam <83418111+shivamgopchade@users.noreply.github.com>
2023-06-24 17:22:26 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -10,14 +10,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDp_g3pGl2GVXyf6AmVQtPC4hH4QrQsfdw",
-  authDomain: "chefmedia-1ce2a.firebaseapp.com",
-  projectId: "chefmedia-1ce2a",
-  storageBucket: "chefmedia-1ce2a.appspot.com",
-  messagingSenderId: "277847103360",
-  appId: "1:277847103360:web:97ba0be84abe15cb9f1365",
-  measurementId: "G-D1YV1QVE3D",
-  databaseURL: "https://chefmedia-1ce2a-default-rtdb.firebaseio.com/",
+  //enter your firebase credentials here
 };
 
 // Initialize Firebase
